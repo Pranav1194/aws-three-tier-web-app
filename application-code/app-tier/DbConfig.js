e802b9ce4628bbe1480002cc1dc4eafeb09a25fb
Pranav1194 <www.sawarkarpranav01@gmail.com>
2025-05-22 16:14:35 +0530

    Initial commit

---
+++ application-code/app-tier/DbConfig.js
@@ -0,0 +1,6 @@
+module.exports = Object.freeze({
+    DB_HOST : 'my-mysql-db.cst20qcs65pi.us-east-1.rds.amazonaws.com',
+    DB_USER : 'admin',
+    DB_PWD : 'Admin1234',
+    DB_DATABASE : 'webappdb'
+});
