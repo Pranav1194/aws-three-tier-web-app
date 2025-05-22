4e3e61d92889b2b16482f0ad65c18fa2fdc5c933
Pranav1194 <156526912+Pranav1194@users.noreply.github.com>
2025-05-22 18:00:10 +0530

    Update DbConfig.js

--- application-code/app-tier/DbConfig.js
+++ application-code/app-tier/DbConfig.js
@@ -1,5 +1,5 @@
 module.exports = Object.freeze({
-    DB_HOST : 'my-mysql-db.cst20qcs65pi.us-east-1.rds.amazonaws.com',
+    DB_HOST : 'database-1.cst20qcs65pi.us-east-1.rds.amazonaws.com',
     DB_USER : 'admin',
     DB_PWD : 'Admin1234',
     DB_DATABASE : 'webappdb'
